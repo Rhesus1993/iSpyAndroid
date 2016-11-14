84a823d1fa09938ac53de13c3e476d19ee90e827
Riley Shelton <rs854@nau.edu>
2016-11-13 22:33:38 -0700

    Fixed the login activity, working on creating the links dynamically, added videos

--- app/src/main/java/com/azshelton/ispy3/Feeds.java
+++ app/src/main/java/com/azshelton/ispy3/Feeds.java
@@ -1,7 +1,16 @@
 package com.azshelton.ispy3;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.TextView;
+import android.widget.VideoView;
+
+import static com.azshelton.ispy3.R.id.text;
+import static com.azshelton.ispy3.R.id.textView;
 
 public class Feeds extends AppCompatActivity {
     public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
@@ -9,5 +18,50 @@ public class Feeds extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feeds);
+        final VideoView videoView = (VideoView) findViewById(R.id.videoView);
+        final TextView textView = (TextView) findViewById(R.id.textView2);
+        Intent intent =getIntent();
+        String str = intent.getStringExtra("location");
+        textView.setText(str);
+        String video = "";
+
+
+        String location = textView.getText().toString();
+        if (location == "Kitchen"){
+            video = "https://www.cefns.nau.edu/~rs854/videos/Kitchen.mp4";
+            videoView.setVideoPath(video);
+        }
+        if (location == "Dining Room"){
+            video = "https://www.cefns.nau.edu/~rs854/videos/Kitchen.mp4";
+            videoView.setVideoPath(video);
+        }
+        if (location == "Patio"){
+            video = "https://www.cefns.nau.edu/~rs854/videos/Patio.mp4";
+            videoView.setVideoPath(video);
+        }
+        if (location =="Hall"){
+            video =  "https://www.cefns.nau.edu/~rs854/videos/Hall.mp4";
+            videoView.setVideoPath(video);
+        }
+        if (location =="Entry"){
+            video = "https://www.cefns.nau.edu/~rs854/videos/Entry.mp4";
+            videoView.setVideoPath(video);
+        }else{
+            video = "https://www.cefns.nau.edu/~rs854/videos/Cat.mp4";
+            videoView.setVideoPath(video);
+        }
+
+        MediaController mediaController = new MediaController(this);
+        mediaController.setAnchorView(videoView);
+        videoView.setMediaController(mediaController);
+
+        videoView.start();
+
+        videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                mp.setLooping(true);
+            }
+        });
     }
 }
--- app/src/main/java/com/azshelton/ispy3/ModifyFeed.java
+++ app/src/main/java/com/azshelton/ispy3/ModifyFeed.java
@@ -1,49 +1,90 @@
 package com.azshelton.ispy3;
 
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
 import android.widget.ToggleButton;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 
 public class ModifyFeed extends AppCompatActivity {
     public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
-    private ToggleButton add;
-    private EditText name;
-    private EditText location;
-    private Button btn1;
+    private ToggleButton addL;
     private Button remove;
+    private Spinner location;
+    String[] videoLocations;
+    int count, i;
+    Button b1[];
+    String current;
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        add = (ToggleButton) findViewById(R.id.add);
-        name = (EditText) findViewById(R.id.editText3);
-        location = (EditText) findViewById(R.id.editText4);
-        btn1 = (Button) findViewById(R.id.btn1);
-        remove = (Button) findViewById(R.id.Remove);
         setContentView(R.layout.activity_modify_feed);
+        remove = (Button)findViewById(R.id.Remove);
+        addL = (ToggleButton)findViewById(R.id.toggleButton);
+        location = (Spinner)findViewById(R.id.spinner1);
+        LinearLayout buttonLayout = (LinearLayout)findViewById(R.id.layout1);
+
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.TEXT_ALIGNMENT_CENTER,LinearLayout.VERTICAL);
+
+        //drop down menu
+        String[] items = new String[]{"Dining Room", "Kitchen", "Patio", "Living Room", "Entry", "Stairway"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        location.setAdapter(adapter);
+
+        videoLocations = new String[count];
+        for(int i = 0; i < videoLocations.length; i++){
+            count++;
+            b1[i] = new Button(this);
+            b1[i].setId(i);
+            b1[i].setText(videoLocations[i]);
+            b1[i].setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+            buttonLayout.addView(b1[i]);
+        }
+
 
     }
+
     public void onClickAdd(View v)
     {
-        if (add.isChecked()){
-            name.setVisibility(View.VISIBLE);
+        if (addL.isChecked()){
             location.setVisibility(View.VISIBLE);
+            current = location.toString();
+            videoLocations[count] = current;
+
+
         }else{
-            name.setVisibility(View.INVISIBLE);
             location.setVisibility(View.INVISIBLE);
+
         }
     }
+
+
     public void onClickRemove(View v)
     {
         //TODO: Make array of buttons and have this remove the last one. Then set the onClickEvent in xml
     }
 
     public void onClickView(View v){
-        Intent mintent = new Intent(ModifyFeed.this, Feeds.class);
-        startActivity(mintent);
+        //Button location = (Button) findViewById(R.id.btn1);
+        //Intent mintent = new Intent(ModifyFeed.this, Feeds.class);
+        //mintent.putExtra("location",location.getText().toString());
+        //startActivity(mintent);
     }
 }
